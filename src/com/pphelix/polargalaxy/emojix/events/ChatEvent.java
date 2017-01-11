b91e15e9f74d5aa82a2baf495e467b0838e13447
Jordan Jones <pphelix@hotmail.com>
2017-01-11 19:39:29 +0000

    Updated permissions support

--- src/com/pphelix/polargalaxy/emojix/events/ChatEvent.java
+++ src/com/pphelix/polargalaxy/emojix/events/ChatEvent.java
@@ -1,6 +1,7 @@
 package com.pphelix.polargalaxy.emojix.events;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -14,35 +15,55 @@ public class ChatEvent implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onChat(AsyncPlayerChatEvent event) {
 		String message = event.getMessage();
+		Player player = (Player) event.getPlayer();
 
-		//TODO: REMOVE UPON RELEASE
+		// TODO: REMOVE UPON RELEASE
 		message = ChatColor.RED + message;
 
 		// This allows us to retain the chat color after we use Emotes
 		String oldColor = ChatColor.getLastColors(message);
-
 		String[] words = message.split(" ");
 
 		for (Object obj : EmojiConfig.emoji.getConfigurationSection("emoji").getKeys(false)) {
 
-			/* This loop allows detection for Emotes mixed with words and also
-			   eliminates Emotes appearing in URL's - Emotes must have a space
-			   on both sides if applicable */
+			/*
+			 * This loop allows detection for Emotes mixed with words and also
+			 * eliminates Emotes appearing in URL's - Emotes must have a space
+			 * on both sides if applicable
+			 */
 			for (String word : words) {
 
 				// This is to allow the first word to be an Emoji should users
 				// use an Emoji at the start
 				if (word == words[0]) {
 					word = ChatColor.stripColor(word);
-					if (word.equals(obj.toString()))
+					if (word.equals(obj.toString())) {
+						if (player.hasPermission("emojix.chat." + word)) {
+							message = message.replace(obj.toString(), ChatColor.WHITE
+									+ EmojiConfig.emoji.getString("emoji." + obj.toString()) + oldColor);
+							continue;
+						} else if (player.hasPermission("emojix.chat.*")) {
+							message = message.replace(obj.toString(), ChatColor.WHITE
+									+ EmojiConfig.emoji.getString("emoji." + obj.toString()) + oldColor);
+							continue;
+						} else {
+							continue;
+						}
+					}
+				}
+
+				if (word.equals(obj.toString())) {
+					if (player.hasPermission("emojix.chat." + word)) {
 						message = message.replace(obj.toString(),
 								ChatColor.WHITE + EmojiConfig.emoji.getString("emoji." + obj.toString()) + oldColor);
-					continue;
-				}
-				
-				if (word.equals(obj.toString())) {
-					message = message.replace(obj.toString(),
-							ChatColor.WHITE + EmojiConfig.emoji.getString("emoji." + obj.toString()) + oldColor);
+						continue;
+					} else if (player.hasPermission("emojix.chat.*")) {
+						message = message.replace(obj.toString(),
+								ChatColor.WHITE + EmojiConfig.emoji.getString("emoji." + obj.toString()) + oldColor);
+						continue;
+					} else {
+						continue;
+					}
 				}
 			}
 		}
